622265eef930752a02db32110be7af926d9af805
Patrik Braborec <patrikbraborec@gmail.com>
2024-11-20 20:12:39 +0100

    Add stock data loader
    Signed-off-by: Patrik Braborec <patrikbraborec@gmail.com>

--- stock-data/index.ts
+++ stock-data/index.ts
@@ -1,11 +1,70 @@
 import { restClient } from "@polygon.io/client-js";
-const rest = restClient(process.env.POLY_API_KEY);
+import { createClient } from "@supabase/supabase-js";
+import { format, subDays, parse } from "date-fns";
 
-rest.stocks
-  .aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.error("An error happened:", e);
-  });
+const TICKER = "AAPL";
+
+const polygonClient = restClient(process.env.POLY_API_KEY);
+const supabaseClient = createClient(
+  process.env.SUPABASE_URL!!,
+  process.env.SUPABASE_KEY!!
+);
+
+(async function () {
+  const yesterday = format(subDays(new Date(), 1), "yyyy-MM-dd");
+
+  try {
+    const stockData = await polygonClient.stocks.dailyOpenClose(
+      TICKER,
+      yesterday
+    );
+    const stockNews = await polygonClient.reference.tickerNews({
+      ticker: TICKER,
+      date: yesterday,
+    });
+
+    const from = format(new Date(), "yyyy-MM-dd");
+    const stockDataToInsert = {
+      ticker: TICKER,
+      timestamp: parse(
+        stockData.from ?? from,
+        "yyyy-MM-dd",
+        new Date()
+      ).toISOString(),
+      open: stockData.open,
+      close: stockData.close,
+      high: stockData.high,
+      low: stockData.low,
+    };
+
+    const stockNewsToInesrt = stockNews?.results?.map((result) => {
+      return {
+        ticker: TICKER,
+        title: result.title,
+        publisher_name: result.publisher.name,
+        author: result.author,
+        article_url: result.article_url,
+        image_url: result.image_url,
+        description: result.description,
+      };
+    });
+
+    const { error: stockDataError } = await supabaseClient
+      .from("stocks")
+      .insert(stockDataToInsert);
+
+    if (stockDataError) {
+      throw stockDataError;
+    }
+
+    const { error: stockNewsError } = await supabaseClient
+      .from("news")
+      .insert(stockNewsToInesrt);
+
+    if (stockNewsError) {
+      throw stockNewsError;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
